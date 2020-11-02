f8f7d9f080a9f7c5b1a9b0f79ca02d3b2a9acfe6
Thomas Martin <thomas@oopss.org>
2020-11-02 20:38:21 +0100

    Populate thread store from reader

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,28 +1,14 @@
 use std::env;
 use rustractor::ThreadStore;
 use rustractor::MemoryThreadStore;
-use rustractor::Message;
 use rustractor::SlackReader;
 
 fn main() {
-    let slack_token = env::var("SLACK_TOKEN").expect("A SLACK_TOKEN environment variable must be defined.");
-    let slack_channel = env::var("SLACK_CHANNEL").expect("A SLACK_CHANNEL environment variable must be defined.");
-    let slack_reader = SlackReader::new(slack_token, slack_channel);
-
     let mut thread = MemoryThreadStore::new();
 
-    let message = Message::new("hey ma gueule ?", "Bernard");
-    thread.add(message);
-
-    let mut message = Message::new("sa va ?", "Bernard");
-
-    // Add subthread
-    let mut thread2 = MemoryThreadStore::new();
-    let message2 = Message::new("ou bien ?", "Bernard");
-    thread2.add(message2);
-    message.add_thread(thread2);
-
-    thread.add(message);
+    let slack_token = env::var("SLACK_TOKEN").expect("A SLACK_TOKEN environment variable must be defined.");
+    let slack_channel = env::var("SLACK_CHANNEL").expect("A SLACK_CHANNEL environment variable must be defined.");
+    let slack_reader = SlackReader::read(&mut thread, slack_token, slack_channel);
 
     thread.output(0);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -63,7 +63,20 @@ const SLACK_URL: &str = "https://slack.com/api";
 pub struct SlackReader{}
 
 impl SlackReader {
-    pub fn new(slack_token: String, slack_channel: String) {
+    pub fn read(thread: &mut Box<MemoryThreadStore>, slack_token: String, slack_channel: String) {
         println!("Connecting to {} channel", slack_channel);
+
+        let message = Message::new("hey ma gueule ?", "Bernard");
+        thread.add(message);
+
+        let mut message = Message::new("sa va ?", "Bernard");
+
+        // Add subthread
+        let mut thread2 = MemoryThreadStore::new();
+        let message2 = Message::new("ou bien ?", "Bernard");
+        thread2.add(message2);
+        message.add_thread(thread2);
+
+        thread.add(message);
     }
 }
