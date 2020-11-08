5bdb9f30342c33b4f68cce9f4ac7cf93934b5a5f
Thomas Martin <thomas@oopss.org>
2020-11-08 22:23:31 +0100

    Update error messages

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,9 +6,9 @@ use rustractor::writer::stdout;
 fn main() {
     let mut thread = MemoryThreadStore::new();
 
-    let slack_token = env::var("SLACK_TOKEN").expect("A SLACK_TOKEN environment variable must be defined.");
-    let slack_channel = env::var("SLACK_CHANNEL").expect("A SLACK_CHANNEL environment variable must be defined.");
-    slack::SlackReader::read(&mut thread, slack_token, slack_channel);
+    let slack_token = env::var("SLACK_TOKEN").expect("SLACK_TOKEN environment variable must be defined.");
+    let slack_conv = env::var("SLACK_CONV").expect("SLACK_CONV environment variable must be defined.");
+    slack::SlackReader::read(&mut thread, &slack_conv, &slack_token);
 
     stdout::StdoutWriter::write(&thread);
 }
