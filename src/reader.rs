4cb576e47249caa2fcb2769b2f755a83e1e2cfc8
Thomas Martin <thomas@oopss.org>
2020-11-23 23:04:16 +0100

    Refactor slack client
    - Add pagination
    - Add ok status check

--- src/reader.rs
+++ src/reader.rs
@@ -1,43 +1,87 @@
 pub mod slack {
     use super::super::Message;
     use super::super::ThreadStore;
-    use serde::de::DeserializeOwned;
-    use serde::{Deserialize, Serialize};
     use serde_json::Value;
     use ureq;
 
     pub struct SlackReader {}
 
-    #[derive(Serialize, Deserialize)]
-    struct SlackConversationListPayload {
-        channels: Vec<Value>,
-    }
-
-    #[derive(Serialize, Deserialize)]
-    struct SlackConversationHistoryPayload {
-        messages: Vec<Value>,
-    }
-
     impl SlackReader {
-        fn slack_get<T>(path: String, slack_token: &String) -> T
-        where
-            T: DeserializeOwned,
-        {
-            let resp = ureq::get(format!("https://slack.com/api/{}", path).as_str())
-                .set("Authorization", format!("Bearer {}", slack_token).as_str())
-                .call();
-            // TODO paginate everything
-            if resp.ok() {
-                // FIXME test slack responds ok
-                let content = resp.into_string().unwrap();
-                serde_json::from_str(&content).unwrap()
-            } else {
-                panic!("Error in Slack response.");
+        fn slack_get<'a>(
+            endpoint: &str,
+            param: &str,
+            resource_key: &str,
+            slack_token: &String,
+        ) -> Vec<Value> {
+            let mut all_ressources: Vec<Value> = Vec::new();
+            let mut next_cursor: Option<String> = None;
+
+            loop {
+                let mut url = format!("https://slack.com/api/{}?{}", endpoint, param);
+                if let Some(ref some_cursor) = next_cursor {
+                    url = format!("{}&cursor={}", url, some_cursor);
+                }
+                println!("Calling URL {}", url);
+                let resp = ureq::get(url.as_str())
+                    .set("Authorization", format!("Bearer {}", slack_token).as_str())
+                    .call();
+                if resp.ok() {
+                    if let Ok(content) = resp.into_string() {
+                        let _payload: Result<Value, _> = serde_json::from_str(&content);
+                        if let Ok(payload) = _payload {
+                            if let Some(ok_status) = payload.get("ok") {
+                                match ok_status {
+                                    Value::Bool(true) => (),
+                                    _ => panic!("Slack ok status error"),
+                                }
+                                if let Some(ressources) = payload.get(resource_key) {
+                                    if let Value::Array(ressources_array) = ressources {
+                                        for ressource in ressources_array {
+                                            all_ressources.push(ressource.to_owned());
+                                        }
+                                    } else {
+                                        panic!("Not array");
+                                    }
+                                    if let Some(response_metadata) =
+                                        payload.get("response_metadata")
+                                    {
+                                        if let Some(_c) = response_metadata.get("next_cursor") {
+                                            if let Value::String(c_str) = _c {
+                                                if c_str.len() > 0 {
+                                                    println!("Paginating to {}", c_str.to_string());
+                                                    next_cursor = Some(c_str.to_string());
+                                                } else {
+                                                    break;
+                                                }
+                                            } else {
+                                                panic!("response_metadata not a string")
+                                            }
+                                        } else {
+                                            break;
+                                        }
+                                    } else {
+                                        break;
+                                    }
+                                } else {
+                                    panic!("No ressource key");
+                                }
+                            } else {
+                                panic!("No ok status in slack response");
+                            }
+                        } else {
+                            panic!("Cannot parse Slack JSON");
+                        }
+                    } else {
+                        panic!("Slack error");
+                    }
+                } else {
+                    panic!("Error in Slack response.");
+                }
             }
+            all_ressources
         }
 
         fn get_conv_info(slack_conv: &String) -> (&str, &str) {
-            // TODO public_channel by default
             let vec: Vec<&str> = slack_conv.split(":").collect::<Vec<&str>>();
             match vec[0] {
                 "public_channel" | "private_channel" | "im" | "mpim" => (vec[0], vec[1]),
@@ -45,7 +89,7 @@ pub mod slack {
             }
         }
 
-        fn get_id_from_channels(channels: &Vec<Value>, conv_name: &str) -> String {
+        fn get_id_from_channels(channels: Vec<Value>, conv_name: &str) -> String {
             for channel in channels {
                 if channel["name"].as_str().unwrap() == conv_name {
                     return String::from(channel["id"].as_str().unwrap());
@@ -56,13 +100,13 @@ pub mod slack {
 
         fn get_conv_id(slack_conv: &String, slack_token: &String) -> String {
             let (conv_type, conv_name) = Self::get_conv_info(slack_conv);
-            let payload: SlackConversationListPayload = Self::slack_get(
-                format!("conversations.list?types={}", conv_type),
+            let payload = Self::slack_get(
+                "conversations.list",
+                format!("types={}", conv_type).as_str(),
+                "channels",
                 slack_token,
             );
-            let conv_id = Self::get_id_from_channels(&payload.channels, conv_name);
-            // TODO debug output
-            println!("Connecting to {} conv from {}", conv_id, slack_conv);
+            let conv_id = Self::get_id_from_channels(payload, conv_name);
             conv_id
         }
 
@@ -71,18 +115,19 @@ pub mod slack {
             conv_id: &String,
             slack_token: &String,
         ) {
-            let payload: SlackConversationHistoryPayload = Self::slack_get(
-                format!("conversations.history?channel={}", conv_id),
+            let payload = Self::slack_get(
+                "conversations.history",
+                format!("channel={}", conv_id).as_str(),
+                "messages",
                 slack_token,
             );
-            for message in payload.messages {
+            for message in payload {
                 let message = Message::new(
                     message["text"].as_str().unwrap(),
                     message["user"].as_str().unwrap(),
                 );
                 thread.add_message(message);
             }
-            // TODO handle threads : curl -F ts=1604256069.047900 -F channel= https://slack.com/api/conversations.replies
         }
 
         pub fn read<T: ThreadStore + 'static>(
