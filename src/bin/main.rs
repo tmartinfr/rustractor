ab15edc462a964476f569f0e9f1a96c6d550ab87
Thomas Martin <thomas@oopss.org>
2020-12-15 18:55:12 +0100

    Improve error handling in main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,31 +5,35 @@ use std::env;
 
 fn main() {
     env_logger::init();
-    if let Ok(slack_token) = env::var("SLACK_TOKEN") {
-        let args: Vec<String> = env::args().collect();
-        if args.len() != 2 {
-            help(Some("Invalid number of arguments."), true);
-        }
-        let first_arg = &args[1];
-        if first_arg == "--help" {
-            help(None, true);
-        }
-        let mut thread = MemoryThreadStore::new();
-        match slack::SlackReader::read(&mut thread, &first_arg, &slack_token) {
-            Ok(()) => {
-                stdout::StdoutWriter::write(&thread);
-            }
-            Err(msg) => help(Some(msg), false),
-        }
-    } else {
-        help(
+
+    let slack_token = match env::var("SLACK_TOKEN") {
+        Ok(token) => token,
+        Err(_) => help(
             Some("SLACK_TOKEN environment variable must be defined"),
             false,
-        );
+        ),
+    };
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        help(Some("Invalid number of arguments."), true);
+    }
+
+    let first_arg = &args[1];
+    if first_arg == "--help" {
+        help(None, true);
+    }
+
+    let mut thread = MemoryThreadStore::new();
+    match slack::SlackReader::read(&mut thread, &first_arg, &slack_token) {
+        Ok(()) => {
+            stdout::StdoutWriter::write(&thread);
+        }
+        Err(msg) => help(Some(msg), false),
     }
 }
 
-fn help(error_message: Option<&str>, usage: bool) {
+fn help(error_message: Option<&str>, usage: bool) -> ! {
     if let Some(msg) = error_message {
         println!("{}", msg);
     }
